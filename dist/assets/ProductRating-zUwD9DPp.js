91f6946b9c722bb07d89ecbad8983dd9aed752da
devtae4 <desarrollo@tecnologiasadministrativas.com>
2025-02-17 12:06:12 -0600

    Inicio con Strapi

---
+++ dist/assets/ProductRating-zUwD9DPp.js
@@ -0,0 +1,1 @@
+import{P as s,R as r,r as o}from"./index-CJahJ6sc.js";const l=({ratingValue:t})=>{let a=[];for(let e=0;e<5;e++)a.push(r.createElement("i",{className:"fa fa-star-o",key:e}));if(t&&t>0)for(let e=0;e<=t-1;e++)a[e]=r.createElement("i",{className:"fa fa-star-o yellow",key:e});return r.createElement(o.Fragment,null,a)};l.propTypes={ratingValue:s.number};export{l as P};
