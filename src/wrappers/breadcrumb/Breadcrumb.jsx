951853dc75b26ed6a92904cf761a4f301655ce95
JesusMario <jmrc1411@gmail.com>
2025-04-04 10:47:44 -0600

    Reducir padding

--- src/wrappers/breadcrumb/Breadcrumb.jsx
+++ src/wrappers/breadcrumb/Breadcrumb.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const BreadcrumbWrap = ({pages}) => {
   return (
-    <div className="breadcrumb-area pt-35 pb-35 bg-gray-3">
+    <div className="breadcrumb-area pt-25 pb-25 bg-gray-6">
       <div className="container">
         <Breadcrumb>
             {pages?.map(({ path, label }, i) => i !== pages.length - 1 ? (
