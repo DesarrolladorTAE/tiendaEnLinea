a4b614f565c2c6fd4675c09aeb5b8d62a9c307b8
JesusMario <jmrc1411@gmail.com>
2025-04-10 16:55:11 -0600

    Eliminar componentes innecesarios del Layout

--- src/layouts/LayoutTEL.jsx
+++ src/layouts/LayoutTEL.jsx
@@ -1,31 +1,14 @@
 import React from "react";
 import { Fragment } from "react";
 import PropTypes from "prop-types";
-import HeaderLogin from "../wrappers/header/HeaderLogin";
-import FooterOne from "../wrappers/footer/FooterOne";
 import ScrollToTop from "../components/scroll-to-top";
 
 const LayoutOne = ({
   children,
-  headerContainerClass = "",
-  headerTop = "",
-  headerPaddingClass = "",
-  headerPositionClass = ""
 }) => {
   return (
     <Fragment>
-      {/* <HeaderLogin
-        layout={headerContainerClass}
-        top={headerTop}
-        headerPaddingClass={headerPaddingClass}
-        headerPositionClass={headerPositionClass}
-      /> */}
       {children}
-      {/* <FooterOne
-        backgroundColorClass="bg-gray"
-        spaceTopClass="pt-100"
-        spaceBottomClass="pb-70"
-      /> */}
       <ScrollToTop />
     </Fragment>
   );
@@ -33,10 +16,6 @@ const LayoutOne = ({
 
 LayoutOne.propTypes = {
   children: PropTypes.node,
-  headerContainerClass: PropTypes.string,
-  headerPaddingClass: PropTypes.string,
-  headerPositionClass: PropTypes.string,
-  headerTop: PropTypes.string
 };
 
 export default LayoutOne;
