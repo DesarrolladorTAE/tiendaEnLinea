b230568e3fd08049190f336ca59614a6b20da8ba
JesusMario <jmrc1411@gmail.com>
2025-04-16 15:25:24 -0600

    Actualizar informacion de los productos

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,29 @@
 // 1. Setup básico
 import React from "react";
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./store/store.jsx";
 import PersistProvider from "./store/providers/persist-provider.jsx";
 import { setProducts } from "./store/slices/product-slice.jsx";
-import 'animate.css';
-import 'swiper/swiper-bundle.min.css';
+import "animate.css";
+import "swiper/swiper-bundle.min.css";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "./assets/scss/style.scss";
 import "./i18n";
 
 // 2. Crea el root ANTES
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 // 3. Fetch de productos desde la API
 const fetchProducts = async () => {
   try {
-    const response = await fetch("https://mitiendaenlineamx.com.mx/api/products", {
+    const response = await fetch("https://mitiendaenlineamx.com.mx/api/productos", {
       headers: {
-        "X-Store-Name": "Tienda Zapatos MX",
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     });
 
     const data = await response.json();
@@ -38,7 +37,6 @@ const fetchProducts = async () => {
     console.error("Error al cargar productos desde la API:", error);
   }
 
-  // 4. Renderiza la app
   root.render(
     <Provider store={store}>
       <PersistProvider>
