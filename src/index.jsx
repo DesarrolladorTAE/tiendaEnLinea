9bf103d3b527d40cdfb1fcc7151bf52c2c32edf0
JesusMario <jmrc1411@gmail.com>
2025-05-26 16:57:37 -0600

    estilos que no se usan

--- src/index.jsx
+++ src/index.jsx
@@ -6,10 +6,10 @@ import { store } from "./store/store.jsx";
 import PersistProvider from "./store/providers/persist-provider.jsx";
 
 // Estilos y librerías
-import "animate.css";
+// import "animate.css";
+// import "yet-another-react-lightbox/styles.css";
+// import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "swiper/swiper-bundle.min.css";
-import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "./assets/scss/style.scss";
 import "./i18n";
 
